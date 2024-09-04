06ab8b9990d3c5ba2b86f844bbceecdc66c30aa1
Marco Alayn <marc_rob92@hotmail.com>
2024-09-04 04:06:27 -0700

    ux/ui improved

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,34 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-
-// // 1. Importamos los hooks de react-redux
-// import { useSelector, useDispatch } from "react-redux";
-
-// // 2. Importamos las acciones
-// import { getAllPokemons } from "../redux/actions";
-import Button from "../components/button/Button";
 import Layout from "../components/layout/Layout";
 
 const Login = () => {
-  // // 3. Creamos las constantes para usar los hooks
-  // const dispatch = useDispatch();
-  // const pokemons = useSelector((state) => state.pokemons);
-
-  // // 4. Creamos la función para llamar a la acción
-  // const getPokemons = () => {
-  //   dispatch(getAllPokemons());
-  // };
-
-  // // utilizando el hook useEffect pintamos
-  // useEffect(() => {
-  //   getPokemons();
-  // }, []);
-
-  return (
-    <Layout>
-      LoginPage
-    </Layout>
-  );
+  return <Layout>LoginPage</Layout>;
 };
 
 export default Login;
